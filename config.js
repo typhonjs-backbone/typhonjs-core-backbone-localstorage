66344e5569e3249e2e53a12f4a8d1a4e941e9f86
typhonrt <support@typhonrt.org>
2016-02-05 03:35:48 -0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  paths: {},
+  map: {}
+});
